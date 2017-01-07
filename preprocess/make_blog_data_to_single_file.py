e8c46dde98a3afae3f1f651bcccb596bc39fb34d
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-01-07 17:59:10 +0900

    move preprocess scripts

--- preprocess/make_blog_data_to_single_file.py
+++ preprocess/make_blog_data_to_single_file.py
@@ -5,7 +5,8 @@ import os
 import pickle
 import re
 
-DATA_DIR = os.path.join(os.path.dirname(os.path.abspath('__file__')), '../data/blog-articles')
+ROWDATA_DIR = os.path.join(os.path.dirname(os.path.abspath('__file__')), '../row_data/blog-articles')
+PREPROCESSED_DIR = os.path.join(os.path.dirname(os.path.realpath('__file__')), 'preprocessed_data/blog-articles')
 
 
 def dump_blog_data(file_list, pickle_filename):
@@ -14,22 +15,25 @@ def dump_blog_data(file_list, pickle_filename):
     for file in file_list:
         blog_dict = {}
 
-        with open(os.path.join(DATA_DIR, file), 'r') as rf:
+        with open(os.path.join(ROWDATA_DIR, file), 'r') as rf:
             reader = csv.reader(rf)
+
             for idx, row in enumerate(reader):
+                blog_dict['date'] = row[0]               # {"date": "ブログ投稿日"}
+                blog_dict['title'] = row[1]              # {"title": "ブログ記事タイトル"}
                 if idx == 0:
-                    blog_dict[row[1]] = []
-                    blog_dict[row[1]].append(row[2])
+                    blog_dict['article'] = []
+                    blog_dict['article'].append(row[2])  # {"article" : "ブログ本文"}
                 else:
-                    blog_dict[row[1]].append(row[2])
+                    blog_dict['article'].append(row[2])
         blog_list.append(blog_dict)
 
-    with open(os.path.join(DATA_DIR, pickle_filename), 'wb') as wf:
+    with open(os.path.join(PREPROCESSED_DIR, pickle_filename), 'wb') as wf:
         pickle.dump(blog_list, wf)
 
 if __name__ == '__main__':
 
-    files = os.listdir(DATA_DIR)
+    files = os.listdir(ROWDATA_DIR)
     # print(files)
 
     pattern = 'ogurayui-0815_.*'
--- preprocess/wakati.py
+++ preprocess/wakati.py
@@ -6,15 +6,13 @@ import os
 import csv
 
 
-DATA_DIR = os.path.join(os.path.dirname(os.path.abspath('__file__')), '../data/blog-articles')
+PREPROCESSED_DIR = os.path.join(os.path.dirname(os.path.abspath('__file__')), 'preprocessed_data/blog-articles')
 
 
 def make_wakati_list(blog_data):
-    wakati_list = []
-    for blog_dict in blog_data:
-        for article_list in blog_dict.values():
-            wakati_string = make_wakati_string(article_list)
-            wakati_list.append(wakati_string)
+
+    wakati_list = [make_wakati_string(blog_dict['article']) for blog_dict in blog_data]
+
     return wakati_list
 
 
@@ -37,25 +35,23 @@ def make_wakati_string(article_list):
 
 if __name__ == '__main__':
 
-    with open(os.path.join(DATA_DIR, 'ogurayui-blog.pkl'), 'rb') as rf:
+    with open(os.path.join(PREPROCESSED_DIR, 'ogurayui-blog.pkl'), 'rb') as rf:
         yui_blog_data = pickle.load(rf)
 
-    with open(os.path.join(DATA_DIR, 'ishiharakaori-blog.pkl'), 'rb') as rf:
+    with open(os.path.join(PREPROCESSED_DIR, 'ishiharakaori-blog.pkl'), 'rb') as rf:
         kaori_blog_data = pickle.load(rf)
 
     yui_wakati_list = make_wakati_list(yui_blog_data)
     kaori_wakati_list = make_wakati_list(kaori_blog_data)
 
-    with open(os.path.join(DATA_DIR, 'wakati-tokens.csv'), 'w') as wf:
+    with open(os.path.join(PREPROCESSED_DIR, 'wakati-tokens.csv'), 'w') as wf:
         writer = csv.writer(wf)
 
-        header = ['label', 'article']
+        header = ['date', 'title', 'article', 'classlabel']
         writer.writerow(header)
 
-        for wakati_string in yui_wakati_list:
-            # print(wakati_string)
-            writer.writerow(["yui", [wakati_string]])
+        for wakati_string, blog_dict in zip(yui_wakati_list, yui_blog_data):
+            writer.writerow([blog_dict['date'], blog_dict['title'], [wakati_string], 'yui'])
 
-        for wakati_string in kaori_wakati_list:
-            # print(wakati_string)
-            writer.writerow(["kaori", [wakati_string]])
+        for wakati_string, blog_dict in zip(kaori_wakati_list, kaori_blog_data):
+            writer.writerow([blog_dict['date'], blog_dict['title'], [wakati_string], 'kaori'])
